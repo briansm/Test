8aeaa3230f132311d92c03aae37bdb24a082c283
GleyP <57022440+GleyP@users.noreply.github.com>
2020-12-08 16:39:30 -0600

    Update test.py
    2do intento commit

--- code/test.py
+++ code/test.py
@@ -19,6 +19,8 @@ class ListaDoble :
     # Agregar nodo al final de la lista [GLEIMY]
     def agregarNodo(self,valorIngresado):
       print ("intento")
+      print ("intento2 ")
+    
         pass
 
     # Buscar un nodo, retorna true/false [JORGE]
