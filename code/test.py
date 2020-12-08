39e87c2d7d683a1a7554d5f2cd49417252de4463
Steve <briansmor09@gmail.com>
2020-12-08 12:06:13 -0600

    Distribuido los roles de trabajo

--- code/test.py
+++ code/test.py
@@ -11,15 +11,35 @@ class ListaDoble :
     
     # Retornar true/false
     def esListaVacia(self) :   
+        if self.primerNodo is None :
+            return True
+        else :
+            return False
+
+    # Agregar nodo al final de la lista [GLEIMY]
+    def agregarNodo(self,valorIngresado):
         pass
 
-    # Agregar nodo a la lista
-    def agregarNodo(self):
+    # Buscar un nodo, retorna true/false [JORGE]
+    def buscarNodo(self,valorABuscar) : 
         pass
 
-    # Buscar un nodo
-    def buscarNodo(self) :
+    # Imprimir Lista: 8->3->5->1 [DIEGO]
+    def mostrarNodosSiguientes(self) : 
         pass
 
-    # Imprimir Lista
-    def mostrarListaDoble(self) :
+    # Imprimir Lista: 1->5->3->8 [EDGAR]
+    def mostrarNodosAnteriores(self) :
+        pass
+
+
+    if __name__ == "__main__":
+        pass
+        # miLista = ListaDoble()
+        # miLista.agregarNodo(8)
+        # miLista.agregarNodo(3)
+        # miLista.agregarNodo(5)
+        # miLista.agregarNodo(1)
+        # print( miLista.buscarNodo(0) )
+        # miLista.mostrarNodosAnteriores()
+        # miLista.mostrarNodosSiguientes()
