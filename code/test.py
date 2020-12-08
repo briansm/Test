4edfb38796b217d7a7ed49eb01941229c8de1930
Steve <briansmor09@gmail.com>
2020-12-08 11:50:23 -0600

    Creada clase listaDoble (metodos sin implementar)

--- code/test.py
+++ code/test.py
@@ -3,3 +3,23 @@ class NodoLista :
         self.valorIngresado = valorIngresado
         self.siguiente = None
         self.anterior = None
+
+class ListaDoble :
+    def __init__(self) :
+        self.primerNodo = None
+        self.ultimoNodo = None
+    
+    # Retornar true/false
+    def esListaVacia(self) :   
+        pass
+
+    # Agregar nodo a la lista
+    def agregarNodo(self):
+        pass
+
+    # Buscar un nodo
+    def buscarNodo(self) :
+        pass
+
+    # Imprimir Lista
+    def mostrarListaDoble(self) :
