e409eb410504ba6fc381a46bf07478de25e770f9
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2020-12-23 17:15:52 -0600

    actualizacion
    extract table y ranged table listos

--- code/llamadaMetodos.py
+++ code/llamadaMetodos.py
@@ -21,30 +21,41 @@ print(imp)
 
 
 #--tablas
-print("\n-----")
+print("\n-----tablas")
 print(createTable("db1","table1",4))
 print(createTable("db1","table2",5))
 print(createTable("db2","table3",9))
 print(createTable("db2","table4",2))
 print(createTable("db3","table5",3))
 
-print(showTables("db1"))
+print(showTables("db5"))
 print(showTables("db2"))
-#print(extractTable("db1", "table1"))
+print("----")
+print(extractTable("db1", "table1"))
 #print(extractRangeTable("bd1","tabla1",2,0,2))
 print(alterTable("db1","table1","tableNew"))
 #alterAddColumn(database, table, columnNumber) :
 #print(alterDropColumn("db1", "table1", 2))
 print(dropTable("db1","table1"))
-print(dropTable("db1","table2"))
-cargarRegistro = loadCSV('Entrada.xls','db1',"Tabla3")
+print(dropTable("db5","table2"))
+print("----finT")
+
+
+
+cargarRegistro = loadCSV('Entrada.csv','db1',"table1")
 print(cargarRegistro)
-reg = extractRow('db1', "table", [4])
+reg = extractRow('db1', "table1", [4])
 print(reg)
-reg2 = extractRow('db1', "table", [25])
+reg2 = extractRow('db1', "table1", [25])
 print(reg2)
 up = update('db1', "table", {1:"ASTERION"}, [25])
 print(up)
 graphTree()
 extraerRegistro = extractRow("database", "table", [25])
 print(extraerRegistro)
+
+
+
+print("----ET")
+print(extractTable("db1", "table1"))
+print("----")
